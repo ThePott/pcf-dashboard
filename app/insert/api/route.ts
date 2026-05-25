513ae930eda10c6bea8e031c13e5e2461b6de143
Tynahan <nusilite@gmail.com>
2026-05-25 12:15:42 +0900

    fix: remove unnecessary api folder

--- app/insert/api/route.ts
+++
@@ -1,7 +0,0 @@
-import prismaClient from "@/app/shared/configs/prisma-client"
-import type { NextApiRequest, NextApiResponse } from "next"
-
-export const findFactorMany = async (_req: NextApiRequest, res: NextApiResponse) => {
-    const result = await prismaClient.emission_factor.findMany()
-    res.status(200).json({ result })
-}
